b6cf337b23a1c031ed59c4911dc44078a6616c26
jjcm296 <jordanjaircruzmendoza@gmail.com>
2025-03-21 23:50:59 -0600

    x

--- frontend/screens/ui/topBar/TopBar.js
+++ frontend/screens/ui/topBar/TopBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, StatusBar } from 'react-native';
-import CoinsDisplay from '../components/CoinsDisplay'; // Monedas
-import StreakDisplay from '../components/StreakDisplay'; // Racha
+import CoinsDisplay from '../components/CoinsDisplay';
+import StreakDisplay from '../components/StreakDisplay';
 
 const TopBar = ({ coins, streak }) => {
     return (
@@ -14,13 +14,12 @@ const TopBar = ({ coins, streak }) => {
 
 const styles = StyleSheet.create({
     topBar: {
-        flexDirection: 'row', // Alinea elementos en fila
-        width: '100%', // Ocupa todo el ancho de la pantalla
-        justifyContent: 'space-between', // Espacia los elementos en la fila
-        alignItems: 'center', // Centra verticalmente
-        paddingHorizontal: 10, // Agrega espacio a los lados
-        paddingVertical: 5, // Agrega espacio arriba y abajo
-        marginTop: StatusBar.currentHeight || 10, // Ajusta la posición debajo de la barra de estado
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        marginTop: StatusBar.currentHeight || 10,
     },
 });
 
