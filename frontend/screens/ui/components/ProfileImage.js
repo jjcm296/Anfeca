420cf122888ec0c22221376b626d44374f4548fa
jjcm296 <jordanjaircruzmendoza@gmail.com>
2025-03-22 18:05:36 -0600

    Set profile image background to transparent

--- frontend/screens/ui/components/ProfileImage.js
+++ frontend/screens/ui/components/ProfileImage.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
-const ProfileImage = ({ source, width = 80, height = 80, borderRadius = 20 }) => {
+const defaultProfileImage = require('../../../assets/sinPerfil.png'); // Ruta de la imagen predeterminada
+
+const ProfileImage = ({ source, width = 80, height = 80, borderRadius = 20, style }) => {
     return (
-        <Image
-            source={source}
-            style={[styles.profileImage, { width, height, borderRadius }]}
-        />
+        <View style={[styles.container, { width, height, borderRadius }, style]}>
+            <Image
+                source={source || defaultProfileImage}
+                style={[
+                    styles.profileImage,
+                    { width: width * 0.8, height: height * 0.8, borderRadius: borderRadius * 0.8 } // Hace la imagen un 80% más chica
+                ]}
+            />
+        </View>
     );
 };
 
+
 const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+        backgroundColor: '#E0E0E0', // Gris claro para el fondo
+    },
     profileImage: {
-        backgroundColor: '#ccc',
+        resizeMode: 'cover',
+        backgroundColor: 'transparent',
+        width:40,
+        
     },
 });
 
